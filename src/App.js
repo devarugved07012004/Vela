c160339d480c86e55d067ac6c29e286fa1e3d056
devarugved07012004 <devarugved@gmail.com>
2023-12-27 20:52:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import About from "./components/About";
+// import About from "./components/About";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React,{useState} from 'react'
-import { BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function App() {
   const [mode,setMode]=useState('light')
@@ -39,29 +38,14 @@ function App() {
     
     <Navbar title="Vela" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
-    <Router>
     <div className="container my-3">
-    <ul className="nav justify-content-end">
-            <li className="nav-item">
-              <Link to="/about" className="nav-link">
-                About
-              </Link>
-            </li>
-          </ul>
     
 
       {/* <TextForm heading="Enter Text:" mode={mode} showAlert={showAlert}/> */}
-      <Routes>
-        <Route path='about' element={<About/>} />
-        
-        <Route path='/' element={<TextForm heading="Enter Text:" mode={mode} showAlert={showAlert}/>} />
-        
-      </Routes>
-
+      <TextForm heading="Enter Text:" mode={mode} showAlert={showAlert}/>
       
     </div>
       {/* <About /> */}
-      </Router>
 
     </>
   );
